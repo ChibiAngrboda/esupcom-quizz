e69b9a3611a48eaee0c15a3d254188694861de1c
Titory0 <titougame00@gmail.com>
2023-11-28 12:22:29 +0100

    2eme

---
+++ Assets/Script/PlayerGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGenerator : MonoBehaviour
+{
+    GameObject player;
+    int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Instantiate(player.transform.position )
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
