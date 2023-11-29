cd1cab61dd801dde38ac71caf432057a145680ea
Titory0 <titougame00@gmail.com>
2023-11-29 09:20:34 +0100

    TIEN LES CHAFGES

--- Assets/Script/PlayerGenerator.cs
+++ Assets/Script/PlayerGenerator.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerGenerator : MonoBehaviour
 {
-    GameObject player;
-    int number;
+    public GameObject team;
+    public GameObject player;
+    public int number;
+    public float distance;
+    public float scaleX;
+    public float scaleY;
     // Start is called before the first frame update
     void Start()
     {
-       // Instantiate(player.transform.position )
+        for (int i = 0; i < number; i++)
+        {
+            Instantiate(team, player.transform.position + Quaternion.Euler(scaleX*90-90, scaleY*90-90, 20 * i) * new Vector3(0, -distance, 0) , transform.rotation);
+        }
     }
 
     // Update is called once per frame
